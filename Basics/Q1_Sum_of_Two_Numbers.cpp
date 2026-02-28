b393b79ee1633641ec783cecbb77e36014121d18
HKohli-1818 <hkohli1818@gmail.com>
2026-02-28 16:34:36 +0530

    basics: sum of two numbers

---
+++ Basics/Q1_Sum_of_Two_Numbers.cpp
@@ -0,0 +1,12 @@
+/*Take two integers as input and print their sum*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cin>>num1>>num2;
+    int sum = num1 + num2;
+    cout<<sum<<endl;
+    return 0;
+}
