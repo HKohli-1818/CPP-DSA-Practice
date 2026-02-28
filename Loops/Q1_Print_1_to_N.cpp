239dd7595dfea16ab5028cb96738723062955b88
HKohli-1818 <hkohli1818@gmail.com>
2026-02-28 17:54:53 +0530

    loops: print 1 to N

---
+++ Loops/Q1_Print_1_to_N.cpp
@@ -0,0 +1,12 @@
+/* Q1: Print numbers from 1 to N */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    for(int i=1;i<=N;i++){
+        cout<<i<<" ";
+    }cout<<endl;
+    return 0;
+}
