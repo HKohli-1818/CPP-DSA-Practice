744c8f5e57fc252169a766cb07621d333096e97e
HKohli-1818 <hkohli1818@gmail.com>
2026-02-28 17:17:47 +0530

    basics:  Divisible by 5 and 11

---
+++ Basics/Q7_Divisible_by_5_and_11.cpp
@@ -0,0 +1,15 @@
+/* Q7: Divisible by 5 and 11 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+    if(num%11==0 && num%5==0){
+        cout<<"Yes\n";
+    }else{
+        cout<<"No\n";
+    }
+    return 0;
+}
