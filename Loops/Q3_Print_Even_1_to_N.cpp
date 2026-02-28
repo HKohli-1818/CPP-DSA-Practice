dd1f38c883f4293c81f5395ec3c4e46fc1b2efd8
HKohli-1818 <hkohli1818@gmail.com>
2026-02-28 18:00:08 +0530

    loops: print even numbers 1 to N

---
+++ Loops/Q3_Print_Even_1_to_N.cpp
@@ -0,0 +1,12 @@
+/* Q3: Print all even numbers from 1 to N */
+
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    for(int i=2;i<=N;i+=2){
+        cout<<i<<" ";
+    }cout<<endl;
+    return 0;
+}
