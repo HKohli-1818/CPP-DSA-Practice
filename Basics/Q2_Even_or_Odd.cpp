346c4e44096050db987443e154f0ffac87a885f7
HKohli-1818 <hkohli1818@gmail.com>
2026-02-28 16:42:24 +0530

    basics: even or odd

---
+++ Basics/Q2_Even_or_Odd.cpp
@@ -0,0 +1,15 @@
+/*Take an integer as input and check whether it is Even or Odd.*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+    if(num%2==0){
+        cout<<"Even\n";
+    }else{
+        cout<<"Odd\n";
+    }
+    return 0;
+}
